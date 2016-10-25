ca3982191fc108c4ae262b3abef6ea34612c91a2
Daniel Castellanos <daniel.castellanos0289@gmail.com>
2016-10-25 15:27:09 -0500

    Revert wrong code change

--- ui/app/Main.js
+++ ui/app/Main.js
@@ -12,7 +12,7 @@ class Main extends Component {
       <Router history={this.props.history}>
         <Route path='/' component={App}>
           <IndexRoute component={Login}/>
-          <Route path="signup/" components={Signup}/>
+          <Route path="/signup" components={Signup}/>
         </Route>
       </Router>
     );
